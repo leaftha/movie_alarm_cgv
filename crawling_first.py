bd62092d22d016880963deba4f9db5107f9ce65c
leaftha <vltpcks@gmail.com>
2020-04-15 03:06:46 +0900

    crawling cheak and error cheak

--- crawling_first.py
+++ crawling_first.py
@@ -1,6 +1,6 @@
 import requests
-from bs4 import BeatifulSoup
+from bs4 import BeautifulSoup
 
-url ='http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?theatercode=P013&screencodes=&screenratingcode=09&regioncode=103'
+url ='http://www.cgv.co.kr/common/showtimes/iframeTheater.aspx?areacode=01&theatercode=P013&date=20200420&screencodes=&screenratingcode=09&regioncode=103'
 html = requests.get(url)
 print(html.text)
